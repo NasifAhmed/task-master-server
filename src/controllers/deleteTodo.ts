0ab137fa6ef6a0a073137bd3834012eb0b84fa91
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-21 13:00:26 +0600

    Fix functions names

--- src/controllers/deleteTodo.ts
+++ src/controllers/deleteTodo.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { removeTodo } from "../services/removeTodo";
 
-export function postTodo(req: Request, res: Response) {
+export function deleteTodo(req: Request, res: Response) {
     removeTodo(req.body).then((response) => {
         return res.send(response);
     });
--- src/controllers/getTodo.ts
+++ src/controllers/getTodo.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { queryTodo } from "../services/queryTodo";
 
-export function postTodo(req: Request, res: Response) {
+export function getTodo(req: Request, res: Response) {
     queryTodo().then((response) => {
         return res.send(response);
     });
